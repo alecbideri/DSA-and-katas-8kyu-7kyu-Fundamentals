91622c561bca0de14bf394fe85d73d5944f1d666
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-01-06 13:35:03 +0200

    new file

---
+++ factory.js
@@ -0,0 +1,13 @@
+function factory(x){
+  return function(array){
+    var newArray = [];
+
+    for(var i = 0 ; i < array.length ; i++){
+      newArray.push(x*(array[i]));
+    }
+
+    return newArray;
+  }
+}
+
+console.log(factory(2)([1,2,3]));
