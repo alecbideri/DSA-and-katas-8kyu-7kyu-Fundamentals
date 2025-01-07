128a5e126c9772b170337f9430f1effe424ef433
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-01-07 12:03:32 +0200

    new file

---
+++ debugCalc.js
@@ -0,0 +1,17 @@
+function calculate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      if (num2 === 0) {
+        return null;
+      }
+      return num1 / num2;
+    default:
+      return null;
+  }
+}
